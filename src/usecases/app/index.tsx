96544c4f0e812a50815dda6eec7304833ffea24e
viniciusflara <viniciusflara@gmail.com>
2022-08-09 21:26:57 -0300

    feat: adding fonts

--- src/usecases/app/index.tsx
+++ src/usecases/app/index.tsx
@@ -3,6 +3,9 @@ import React from 'react'
 import { ThemeProvider, NotifyProvider, AuthProvider } from '../../providers'
 import { Props } from './dto'
 
+import '../../styles/fonts/Inter/styles.css'
+import '../../styles/fonts/Rubik/styles.css'
+
 export const App: React.FC<Props> = ({ color, ...auth }) => {
     return (
         <ThemeProvider color={color}>
